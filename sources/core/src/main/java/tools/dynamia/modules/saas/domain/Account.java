9c902ae63158b55ecfd4d0430c54f9bd5d84fc86
Mario Serrano <mario@dynamiasoluciones.com>
2016-02-22 11:33:06 -0500

    mejoras

--- sources/core/src/main/java/tools/dynamia/modules/saas/domain/Account.java
+++ sources/core/src/main/java/tools/dynamia/modules/saas/domain/Account.java
@@ -29,196 +29,196 @@ import tools.dynamia.modules.saas.api.enums.AccountStatus;
 @Table(name = "saas_accounts")
 public class Account extends SimpleEntity {
 
-	@NotNull
-	@NotEmpty(message = "ingrese nombre de cuenta")
-	@Column(unique = true)
-	private String name;
-	@NotNull
-	@Column(unique = true)
-	private String subdomain;
-	private String customDomain;
-	@NotNull
-	@Email(message = "ingreso direccion de correo valida ")
-	@NotEmpty(message = "ingrese direccion de correo electronico")
-	private String email;
-	@OneToOne
-	@NotNull
-	private AccountType type;
-	@Temporal(javax.persistence.TemporalType.DATE)
-	private Date expirationDate;
-	private AccountStatus status;
-	@Temporal(javax.persistence.TemporalType.TIMESTAMP)
-	private Date statusDate;
-	private String statusDescription;
-	@Temporal(javax.persistence.TemporalType.TIMESTAMP)
-	private Date creationDate = new Date();
-	private boolean defaultAccount;
-	private String skin;
-	@OneToOne
-	private EntityFile logo;
-	private String locale;
-	private String timeZone;
-	@OneToOne
-	private AccountProfile profile;
+    @NotNull
+    @NotEmpty(message = "ingrese nombre de cuenta")
+    @Column(unique = true)
+    private String name;
+    @NotNull
+    @Column(unique = true)
+    private String subdomain;
+    private String customDomain;
+    @NotNull
+    @Email(message = "ingreso direccion de correo valida ")
+    @NotEmpty(message = "ingrese direccion de correo electronico")
+    private String email;
+    @OneToOne
+    @NotNull
+    private AccountType type;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date expirationDate;
+    private AccountStatus status;
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date statusDate;
+    private String statusDescription;
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date creationDate = new Date();
+    private boolean defaultAccount;
+    private String skin;
+    @OneToOne
+    private EntityFile logo;
+    private String locale;
+    private String timeZone;
+    @OneToOne
+    private AccountProfile profile;
 
-	public AccountProfile getProfile() {
-		return profile;
-	}
+    public AccountProfile getProfile() {
+        return profile;
+    }
 
-	public void setProfile(AccountProfile profile) {
-		this.profile = profile;
-	}
+    public void setProfile(AccountProfile profile) {
+        this.profile = profile;
+    }
 
-	public String getTimeZone() {
-		return timeZone;
-	}
+    public String getTimeZone() {
+        return timeZone;
+    }
 
-	public void setTimeZone(String timeZone) {
-		this.timeZone = timeZone;
-	}
+    public void setTimeZone(String timeZone) {
+        this.timeZone = timeZone;
+    }
 
-	public String getLocale() {
-		return locale;
-	}
+    public String getLocale() {
+        return locale;
+    }
 
-	public void setLocale(String locale) {
-		this.locale = locale;
-	}
+    public void setLocale(String locale) {
+        this.locale = locale;
+    }
 
-	public String getSkin() {
-		return skin;
-	}
+    public String getSkin() {
+        return skin;
+    }
 
-	public void setSkin(String skin) {
-		this.skin = skin;
-	}
+    public void setSkin(String skin) {
+        this.skin = skin;
+    }
 
-	public EntityFile getLogo() {
-		return logo;
-	}
+    public EntityFile getLogo() {
+        return logo;
+    }
 
-	public void setLogo(EntityFile logo) {
-		this.logo = logo;
-	}
+    public void setLogo(EntityFile logo) {
+        this.logo = logo;
+    }
 
-	public boolean isDefaultAccount() {
-		return defaultAccount;
-	}
+    public boolean isDefaultAccount() {
+        return defaultAccount;
+    }
 
-	public void setDefaultAccount(boolean defaultAccount) {
-		this.defaultAccount = defaultAccount;
-	}
+    public void setDefaultAccount(boolean defaultAccount) {
+        this.defaultAccount = defaultAccount;
+    }
 
-	public String getCustomDomain() {
-		return customDomain;
-	}
+    public String getCustomDomain() {
+        return customDomain;
+    }
 
-	public void setCustomDomain(String customDomain) {
-		this.customDomain = customDomain;
-	}
+    public void setCustomDomain(String customDomain) {
+        this.customDomain = customDomain;
+    }
 
-	public Date getCreationDate() {
-		return creationDate;
-	}
+    public Date getCreationDate() {
+        return creationDate;
+    }
 
-	public void setCreationDate(Date creationDate) {
-		this.creationDate = creationDate;
-	}
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getSubdomain() {
-		return subdomain;
-	}
+    public String getSubdomain() {
+        return subdomain;
+    }
 
-	public void setSubdomain(String subdomain) {
-		this.subdomain = subdomain;
-	}
+    public void setSubdomain(String subdomain) {
+        this.subdomain = subdomain;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public AccountType getType() {
-		return type;
-	}
+    public AccountType getType() {
+        return type;
+    }
 
-	public void setType(AccountType type) {
-		this.type = type;
-	}
+    public void setType(AccountType type) {
+        this.type = type;
+    }
 
-	public Date getExpirationDate() {
-		return expirationDate;
-	}
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
 
-	public void setExpirationDate(Date expirationDate) {
-		this.expirationDate = expirationDate;
-	}
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
 
-	public AccountStatus getStatus() {
-		return status;
-	}
+    public AccountStatus getStatus() {
+        return status;
+    }
 
-	public void setStatus(AccountStatus status) {
-		if (status != this.status) {
-			setStatusDate(new Date());
-		}
-		this.status = status;
-	}
+    public void setStatus(AccountStatus status) {
+        if (status != this.status) {
+            setStatusDate(new Date());
+        }
+        this.status = status;
+    }
 
-	public Date getStatusDate() {
-		return statusDate;
-	}
+    public Date getStatusDate() {
+        return statusDate;
+    }
 
-	public void setStatusDate(Date statusDate) {
-		this.statusDate = statusDate;
-	}
+    public void setStatusDate(Date statusDate) {
+        this.statusDate = statusDate;
+    }
 
-	public String getStatusDescription() {
-		return statusDescription;
-	}
+    public String getStatusDescription() {
+        return statusDescription;
+    }
 
-	public void setStatusDescription(String statusDescription) {
-		this.statusDescription = statusDescription;
-	}
+    public void setStatusDescription(String statusDescription) {
+        this.statusDescription = statusDescription;
+    }
 
-	@Override
-	public String toString() {
-		return String.format("%s (%s)", getName(), getEmail());
-	}
+    @Override
+    public String toString() {
+        return String.format("%s (%s)", getName(), getEmail());
+    }
 
-	public AccountInfo getInfo() {
-		String logoURL = null;
-		if (logo != null) {
-			logoURL = logo.getStoredEntityFile().getThumbnailUrl(200, 200);
-		}
+    public AccountInfo getInfo() {
+        String logoURL = null;
+        if (logo != null) {
+            logoURL = logo.getStoredEntityFile().getThumbnailUrl(200, 200);
+        }
 
-		AccountInfo info = new AccountInfo(
-				getId(),
-				name,
-				email,
-				status,
-				getType().getPeriodicity(),
-				getType().getName(),
-				creationDate,
-				subdomain,
-				customDomain,
-				statusDescription,
-				skin,
-				logoURL,
-				locale,
-				timeZone,
-				getType().getMaxUsers());
-		return info;
-	}
+        AccountInfo info = new AccountInfo(
+                getId(),
+                name,
+                email,
+                status,
+                getType().getPeriodicity(),
+                getType().getName(),
+                creationDate,
+                subdomain,
+                customDomain,
+                statusDescription,
+                skin,
+                logoURL,
+                locale,
+                timeZone,
+                getType().getMaxUsers());
+        return info;
+    }
 }
---
+++ sources/ui/src/main/java/tools/dynamia/modules/saas/ui/action/ViewAccountLogoAction.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tools.dynamia.modules.saas.ui.action;
+
+import org.zkoss.zul.Div;
+import tools.dynamia.actions.InstallAction;
+import tools.dynamia.commons.ApplicableClass;
+import tools.dynamia.crud.AbstractCrudAction;
+import tools.dynamia.crud.CrudActionEvent;
+import tools.dynamia.crud.CrudState;
+import tools.dynamia.modules.entityfile.ui.components.EntityFileImage;
+import tools.dynamia.modules.saas.domain.Account;
+import tools.dynamia.ui.MessageType;
+import tools.dynamia.ui.UIMessages;
+import tools.dynamia.zk.util.ZKUtil;
+
+/**
+ *
+ * @author mario
+ */
+@InstallAction
+public class ViewAccountLogoAction extends AbstractCrudAction {
+
+    public ViewAccountLogoAction() {
+        setName("View Logo");
+        setImage("photo");
+    }
+
+    @Override
+    public void actionPerformed(CrudActionEvent evt) {
+        Account account = (Account) evt.getData();
+        if (account != null) {
+            showLogo(account);
+        } else {
+            UIMessages.showMessage("Seleccione cuenta para ver logo", MessageType.WARNING);
+        }
+    }
+
+    @Override
+    public ApplicableClass[] getApplicableClasses() {
+        return ApplicableClass.get(Account.class);
+    }
+
+    @Override
+    public CrudState[] getApplicableStates() {
+        return CrudState.get(CrudState.READ);
+    }
+
+    private void showLogo(Account account) {
+        if (account.getLogo() == null) {
+            UIMessages.showMessage("La cuenta seleccionada no tiene logo", MessageType.WARNING);
+        } else {
+            System.out.println(account.getLogo().getStoredEntityFile().getThumbnailUrl());
+            EntityFileImage image = new EntityFileImage();
+            image.setValue(account.getLogo());
+
+            Div div = new Div();
+            div.setHflex("1");
+            div.setVflex("1");
+            div.setStyle("overflow:auto;background:silver");
+
+            div.appendChild(image);
+
+            ZKUtil.showDialog("Logo " + account.getName(), div, "600px", "500px");
+
+        }
+    }
+
+}
