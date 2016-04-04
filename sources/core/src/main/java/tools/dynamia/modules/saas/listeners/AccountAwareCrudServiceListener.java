11b756582ff3b3bdfe44866f2deeeaeb4f93f817
Mario Serrano <mario@dynamiasoluciones.com>
2016-04-04 13:11:30 -0500

    mejoras

--- sources/core/src/main/java/tools/dynamia/modules/saas/listeners/AccountAwareCrudServiceListener.java
+++ sources/core/src/main/java/tools/dynamia/modules/saas/listeners/AccountAwareCrudServiceListener.java
@@ -20,33 +20,36 @@ import tools.dynamia.modules.saas.domain.Account;
  */
 @Component
 public class AccountAwareCrudServiceListener extends CrudServiceListenerAdapter<AccountAware> {
+    
+    private static final String ACCOUNT_ID = "accountId";
 
-	@Override
-	public void beforeCreate(AccountAware entity) {
-		if (entity.getAccountId() == null) {
-			try {
-				entity.setAccountId(AccountContext.getCurrent().getAccount().getId());
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
+    @Override
+    public void beforeCreate(AccountAware entity) {
+        if (entity.getAccountId() == null) {
+            try {
+                entity.setAccountId(AccountContext.getCurrent().getAccount().getId());
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
 
-	@Override
-	public void beforeQuery(QueryParameters params) {
-		if (!params.containsKey("accountId")) {
-			Class paramsType = params.getType();
-			if (paramsType != null) {
-				Object obj = BeanUtils.newInstance(paramsType);
-				if (obj instanceof AccountAware) {
-					Account account = AccountContext.getCurrent().getAccount();
-					if (account != null) {						
-						params.add("accountId", account.getId());
-					}
-				}
-			}
-		}
-	}
+    @Override
+    public void beforeQuery(QueryParameters params) {
+        if (!params.containsKey(ACCOUNT_ID) || params.get(ACCOUNT_ID) == null) {
+            Class paramsType = params.getType();
+            if (paramsType != null) {
+                Object obj = BeanUtils.newInstance(paramsType);
+                if (obj instanceof AccountAware) {
+                    Account account = AccountContext.getCurrent().getAccount();
+                    if (account != null) {
+                        params.add(ACCOUNT_ID, account.getId());
+                    }
+                }
+            }
+        }
+    }
+    
 
 }
