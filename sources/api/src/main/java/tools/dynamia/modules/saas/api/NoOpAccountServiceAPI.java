a83b59b24bd27828e617491fad0ff8baf0dcdaaa
Mario Serrano <mario@dynamiasoluciones.com>
2016-07-29 12:18:11 -0500

    no operation api implementation

---
+++ sources/api/src/main/java/tools/dynamia/modules/saas/api/NoOpAccountServiceAPI.java
@@ -0,0 +1,40 @@
+package tools.dynamia.modules.saas.api;
+
+import java.util.Date;
+
+import tools.dynamia.modules.saas.api.enums.AccountPeriodicity;
+import tools.dynamia.modules.saas.api.enums.AccountStatus;
+
+public class NoOpAccountServiceAPI implements AccountServiceAPI {
+
+	private static final Long ACCOUNT_ID = 1L;
+	private static final AccountInfo CURRENT_ACCOUNT = new AccountInfo(ACCOUNT_ID, "", "1", "account@api.com",
+			AccountStatus.ACTIVE, AccountPeriodicity.UNLIMITED, "NoOp", new Date(), "", null, null, null, null, null,
+			"GMT-5", 10000, true, 1, null, null);
+
+	@Override
+	public AccountStatus getAccountStatus(Long accountId) {
+		return AccountStatus.ACTIVE;
+	}
+
+	@Override
+	public AccountInfo getAccountInfo(Long accountId) {
+		return CURRENT_ACCOUNT;
+	}
+
+	@Override
+	public Long getSystemAccountId() {
+		return ACCOUNT_ID;
+	}
+
+	@Override
+	public Long getCurrentAccountId() {
+		return ACCOUNT_ID;
+	}
+
+	@Override
+	public void updateAccountUsers(Long accountId, long users, long activedUsers) {
+
+	}
+
+}
