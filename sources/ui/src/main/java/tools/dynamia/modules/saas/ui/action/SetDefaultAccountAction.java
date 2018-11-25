9a20bcc19c35d577335f5a89996135455be2f3fa
Mario Serrano <mario@dynamiasoluciones.com>
2018-11-25 06:29:29 -0500

    actualizado encabezado de licencia

--- sources/ui/src/main/java/tools/dynamia/modules/saas/ui/action/SetDefaultAccountAction.java
+++ sources/ui/src/main/java/tools/dynamia/modules/saas/ui/action/SetDefaultAccountAction.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tools.dynamia.modules.saas.ui.action;
 
 import org.springframework.beans.factory.annotation.Autowired;
