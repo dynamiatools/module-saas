ce0cc4c920ef035c4bc6106940cea249203cd03b
Mario Serrano <mario@dynamiasoluciones.com>
2018-12-10 04:11:06 -0500

    improvements and fixes

--- sources/core/src/main/java/tools/dynamia/modules/saas/AccountContext.java
+++ sources/core/src/main/java/tools/dynamia/modules/saas/AccountContext.java
@@ -1,86 +1,81 @@
 
 package tools.dynamia.modules.saas;
 
-import java.util.List;
-
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import tools.dynamia.commons.logger.LoggingService;
 import tools.dynamia.commons.logger.SLF4JLoggingService;
 import tools.dynamia.integration.Containers;
 import tools.dynamia.modules.saas.domain.Account;
 import tools.dynamia.modules.saas.services.AccountService;
+import tools.dynamia.web.util.HttpUtils;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
 
 /**
- *
  * @author Mario Serrano Leones
  */
 @Component("accountContext")
 public class AccountContext {
 
-	private final LoggingService logger = new SLF4JLoggingService(AccountContext.class);
+    private final LoggingService logger = new SLF4JLoggingService(AccountContext.class);
 
-	@Autowired
-	private List<AccountResolver> resolvers;
+    @Autowired
+    private List<AccountResolver> resolvers;
 
-	@Autowired
-	private AccountService service;
+    @Autowired
+    private AccountService service;
 
-	public static AccountContext getCurrent() {
-		return Containers.get().findObject(AccountContext.class);
-	}
 
-	public Account getAccount() {
-		Account account = null;
-		try {
-			account = AccountSessionHolder.get().getCurrent();
-		} catch (Exception e) {
-			logger.warn("Cannot get AccountSessionHolder: " + e.getMessage());
-		}
-		if (account == null) {
-			for (AccountResolver resolver : resolvers) {
-				account = resolver.resolve();
-				if (account != null) {
-					break;
-				}
-			}
-		}
+    public static AccountContext getCurrent() {
+        return Containers.get().findObject(AccountContext.class);
+    }
 
-		if (account == null) {
-			account = service.getDefaultAccount();
-			if (account == null) {
-				account = service.init();
-			}
+    public Account getAccount() {
+        Account account = null;
 
-		}
 
-		try {
-			AccountSessionHolder.get().setCurrent(account);
-		} catch (Exception e) {
-			logger.warn("Cannot get AccountSessionHolder: " + e.getMessage());
-		}
+        if(HttpUtils.isInWebScope()){
+            account = AccountSessionHolder.get().getCurrent();
+        }
 
-		return account;
-	}
+        if (account == null) {
+            for (AccountResolver resolver : resolvers) {
+                account = resolver.resolve();
+                if (account != null) {
+                    break;
+                }
+            }
+        }
 
-	public boolean isAdminAccount() {
-		Account account = getAccount();
-		if (account != null) {
-			return account.getType().getName().equalsIgnoreCase("admin");
-		}
-		return false;
-	}
+        if (account != null && HttpUtils.isInWebScope()) {
+            AccountSessionHolder.get().setCurrent(account);
+        }
+        return account;
+    }
 
-	@PostConstruct
-	private void fixEntities() {
-		try {
-			service.fixAccountAwareEntities();
-		} catch (Exception e) {
-		}
+    public boolean isAdminAccount() {
+        Account account = getAccount();
+        if (account != null) {
+            return account.getType().getName().equalsIgnoreCase("admin");
+        }
+        return false;
+    }
 
-	}
+    @PostConstruct
+    private void fixEntities() {
+        try {
+
+            Account defaultAccount = service.getDefaultAccount();
+            if (defaultAccount == null) {
+                defaultAccount = service.init();
+            }
+
+            service.fixAccountAwareEntities();
+        } catch (Exception e) {
+        }
+
+    }
 
 }
--- sources/core/src/main/java/tools/dynamia/modules/saas/listeners/AccountAwareCrudServiceListener.java
+++ sources/core/src/main/java/tools/dynamia/modules/saas/listeners/AccountAwareCrudServiceListener.java
@@ -2,7 +2,6 @@
 package tools.dynamia.modules.saas.listeners;
 
 import org.springframework.stereotype.Component;
-
 import tools.dynamia.commons.BeanUtils;
 import tools.dynamia.domain.query.QueryParameters;
 import tools.dynamia.domain.util.CrudServiceListenerAdapter;
@@ -11,12 +10,11 @@ import tools.dynamia.modules.saas.api.AccountAware;
 import tools.dynamia.modules.saas.domain.Account;
 
 /**
- *
  * @author Mario Serrano Leones
  */
 @Component
 public class AccountAwareCrudServiceListener extends CrudServiceListenerAdapter<AccountAware> {
-    
+
     private static final String ACCOUNT_ID = "accountId";
 
     @Override
@@ -33,7 +31,7 @@ public class AccountAwareCrudServiceListener extends CrudServiceListenerAdapter<
 
     @Override
     public void beforeQuery(QueryParameters params) {
-        if (!params.containsKey(ACCOUNT_ID) || params.get(ACCOUNT_ID) == null) {
+        if (!params.containsKey(ACCOUNT_ID) || params.get(ACCOUNT_ID) == null || params.get(ACCOUNT_ID).equals(0L)) {
             Class paramsType = params.getType();
             if (paramsType != null) {
                 Object obj = BeanUtils.newInstance(paramsType);
@@ -46,6 +44,6 @@ public class AccountAwareCrudServiceListener extends CrudServiceListenerAdapter<
             }
         }
     }
-    
+
 
 }
