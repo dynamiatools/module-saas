8f9c6112c27d643272cf94b949aa337c3eeecf38
Mario Serrano <mario@dynamiasoluciones.com>
2019-02-28 16:30:33 -0500

    mejoras

---
+++ sources/ui/src/main/java/tools/dynamia/modules/saas/ui/HelpSystemNavigationRestriction.java
@@ -0,0 +1,37 @@
+package tools.dynamia.modules.saas.ui;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import tools.dynamia.modules.saas.api.AccountServiceAPI;
+import tools.dynamia.modules.saas.api.dto.AccountDTO;
+import tools.dynamia.navigation.NavigationElement;
+import tools.dynamia.navigation.NavigationRestriction;
+
+@Component
+public class HelpSystemNavigationRestriction implements NavigationRestriction {
+
+    @Autowired
+    private AccountServiceAPI accountServiceAPI;
+
+    @Override
+    public Boolean allowAccess(NavigationElement element) {
+        if (element.getVirtualPath().startsWith("system/help")) {
+            Long accountId = accountServiceAPI.getCurrentAccountId();
+            if (accountId != null) {
+                AccountDTO accountDTO = accountServiceAPI.getAccount(accountId);
+                if (accountDTO != null && !accountDTO.getType().equals("admin")) {
+                    return false;
+                }
+            }
+
+        }
+        return null;
+    }
+
+    @Override
+    public int getOrder() {
+
+        return 0;
+    }
+
+}
