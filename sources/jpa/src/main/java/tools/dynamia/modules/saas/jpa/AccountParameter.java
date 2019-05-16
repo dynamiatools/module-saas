5a325d098d775c0ee80b77cb02ecda9d9b35236c
Mario Serrano <mario@dynamiasoluciones.com>
2019-05-16 12:04:35 -0500

    licensed using lgpl v3

--- sources/jpa/src/main/java/tools/dynamia/modules/saas/jpa/AccountParameter.java
+++ sources/jpa/src/main/java/tools/dynamia/modules/saas/jpa/AccountParameter.java
@@ -1,6 +1,28 @@
 
 package tools.dynamia.modules.saas.jpa;
 
+/*-
+ * #%L
+ * DynamiaModules - SaaS JPA
+ * %%
+ * Copyright (C) 2016 - 2019 Dynamia Soluciones IT SAS
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
 import tools.dynamia.domain.jpa.JpaParameter;
 import tools.dynamia.domain.query.QueryConditions;
 import tools.dynamia.domain.query.QueryParameters;
