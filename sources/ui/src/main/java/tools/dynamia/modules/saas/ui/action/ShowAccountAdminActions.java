bb4203039e7ba954d6f8e4c2884de797601ddd05
Mario Serrano <mario@dynamiasoluciones.com>
2020-09-08 23:47:03 -0500

    mis ventas

--- sources/ui/src/main/java/tools/dynamia/modules/saas/ui/action/ShowAccountAdminActions.java
+++ sources/ui/src/main/java/tools/dynamia/modules/saas/ui/action/ShowAccountAdminActions.java
@@ -10,12 +10,12 @@ package tools.dynamia.modules.saas.ui.action;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -26,11 +26,7 @@ import org.zkoss.zk.ui.Component;
 import org.zkoss.zul.Button;
 import org.zkoss.zul.Vlayout;
 
-import tools.dynamia.actions.ActionEvent;
-import tools.dynamia.actions.ActionEventBuilder;
-import tools.dynamia.actions.ActionLoader;
-import tools.dynamia.actions.ActionRenderer;
-import tools.dynamia.actions.InstallAction;
+import tools.dynamia.actions.*;
 import tools.dynamia.commons.ApplicableClass;
 import tools.dynamia.crud.AbstractCrudAction;
 import tools.dynamia.crud.CrudActionEvent;
@@ -43,52 +39,56 @@ import tools.dynamia.ui.UIMessages;
 import tools.dynamia.zk.actions.ButtonActionRenderer;
 import tools.dynamia.zk.util.ZKUtil;
 
+import java.util.Comparator;
+
 @InstallAction
 public class ShowAccountAdminActions extends AbstractCrudAction {
 
-	public ShowAccountAdminActions() {
-		setName("Admin");
-		setImage("settings");
-	}
+    public ShowAccountAdminActions() {
+        setName("Admin");
+        setImage("settings");
+    }
 
-	@Override
-	public CrudState[] getApplicableStates() {
-		return CrudState.get(CrudState.READ);
-	}
+    @Override
+    public CrudState[] getApplicableStates() {
+        return CrudState.get(CrudState.READ);
+    }
 
-	@Override
-	public ApplicableClass[] getApplicableClasses() {
-		return ApplicableClass.get(Account.class);
-	}
+    @Override
+    public ApplicableClass[] getApplicableClasses() {
+        return ApplicableClass.get(Account.class);
+    }
 
-	@Override
-	public void actionPerformed(CrudActionEvent evt) {
-		Account account = (Account) evt.getData();
-		if (account != null) {
-			AccountDTO info = account.toDTO();
-			ActionEventBuilder evtBuilder = (source, params) -> new ActionEvent(info, this);
+    @Override
+    public void actionPerformed(CrudActionEvent evt) {
+        Account account = (Account) evt.getData();
+        if (account != null) {
+            AccountDTO info = account.toDTO();
+            ActionEventBuilder evtBuilder = (source, params) -> new ActionEvent(info, this);
 
-			ActionLoader<AccountAdminAction> loader = new ActionLoader<>(AccountAdminAction.class);
-			loader.setIgnoreRestrictions(true);
-			Vlayout layout = new Vlayout();
-			layout.setHflex("1");
-			ButtonActionRenderer defaultRenderer = new ButtonActionRenderer();
-			
-			loader.load().forEach(a -> {
-				ActionRenderer renderer = a.getRenderer() == null ? defaultRenderer : a.getRenderer();
-				Object component = renderer.render(a, evtBuilder);
-				if(component instanceof Button){
-					((Button) component).setZclass("btn btn-primary btn-block");
-				}
-				layout.appendChild((Component) component);
-			});
+            ActionLoader<AccountAdminAction> loader = new ActionLoader<>(AccountAdminAction.class);
+            loader.setIgnoreRestrictions(true);
+            Vlayout layout = new Vlayout();
+            layout.setHflex("1");
+            ButtonActionRenderer defaultRenderer = new ButtonActionRenderer();
 
-			ZKUtil.showDialog("Actions for " + info.getName(), layout, "600px", "500px");
+            var actions = loader.load();
+            actions.sort(Comparator.comparing(Action::getName));
+            actions.forEach(a -> {
+                ActionRenderer renderer = a.getRenderer() == null ? defaultRenderer : a.getRenderer();
+                Object component = renderer.render(a, evtBuilder);
+                if (component instanceof Button) {
+                    ((Button) component).setZclass("btn btn-primary btn-block");
+                }
+                layout.appendChild((Component) component);
+            });
 
-		} else {
-			UIMessages.showMessage("Select account", MessageType.WARNING);
-		}
+            ZKUtil.showDialog("Actions for " + info.getName(), layout, "600px", "500px");
 
-	}
+        } else {
+            UIMessages.showMessage("Select account", MessageType.WARNING);
+        }
+
+    }
 
 }
